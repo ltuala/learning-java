4e79c87300c0de1d0fec149b88c30af1e281eed7
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-08 17:30:55 +0800

    add a person and student object and explore inheritance

---
+++ object-oriented-programming-2/src/com/learningjava/oops/level2/inheritance/StudentRunner.java
@@ -0,0 +1,9 @@
+package com.learningjava.oops.level2.inheritance;
+
+public class StudentRunner {
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.setName("Leo");
+		student.setEmail("leo@gmail.com");
+	}
+}
