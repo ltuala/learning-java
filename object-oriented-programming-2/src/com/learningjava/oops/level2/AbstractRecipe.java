54f3da88ae38b1ee87e7369f9fcf247fe596db07
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-09 07:55:25 +0800

    explore the use of abstract class

---
+++ object-oriented-programming-2/src/com/learningjava/oops/level2/AbstractRecipe.java
@@ -0,0 +1,19 @@
+package com.learningjava.oops.level2;
+
+public abstract class AbstractRecipe {
+
+	public void execute() {
+		getReady();
+		doTheDish();
+		cleanup();
+	}
+
+	abstract void getReady();
+
+	abstract void doTheDish();
+
+	abstract void cleanup();
+	// prepare
+	// recipe
+	// cleanup
+}
---
+++ object-oriented-programming-2/src/com/learningjava/oops/level2/Recipe1.java
@@ -0,0 +1,21 @@
+package com.learningjava.oops.level2;
+
+public class Recipe1 extends AbstractRecipe {
+
+	@Override
+	void getReady() {
+		System.out.println("Get the raw materials");
+		System.out.println("Get the utensils");
+	}
+
+	@Override
+	void doTheDish() {
+		System.out.println("Prepare the dish");
+	}
+
+	@Override
+	void cleanup() {
+		System.out.println("Cleanup the utensils");
+	}
+
+}
