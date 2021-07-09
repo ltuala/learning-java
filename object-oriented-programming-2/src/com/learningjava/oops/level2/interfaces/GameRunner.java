9fb8fbc2d83eb01a695eb93e279273b2ae16fcd4
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-09 08:20:12 +0800

    add files about interfaces

---
+++ object-oriented-programming-2/src/com/learningjava/oops/level2/interfaces/GameRunner.java
@@ -0,0 +1,12 @@
+package com.learningjava.oops.level2.interfaces;
+
+public class GameRunner {
+	public static void main(String[] args) {
+//		GamingConsole game = new MarioGame();
+		GamingConsole game = new ChessGame();
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+	}
+}
---
+++ object-oriented-programming-2/src/com/learningjava/oops/level2/interfaces/MarioGame.java
@@ -0,0 +1,25 @@
+package com.learningjava.oops.level2.interfaces;
+
+public class MarioGame implements GamingConsole {
+
+	@Override
+	public void up() {
+		System.out.println("Jump");
+	}
+
+	@Override
+	public void down() {
+		System.out.println("Goes into a hole");
+	}
+
+	@Override
+	public void left() {
+		System.out.println("Backward");
+	}
+
+	@Override
+	public void right() {
+		System.out.println("Forward");
+	}
+
+}
