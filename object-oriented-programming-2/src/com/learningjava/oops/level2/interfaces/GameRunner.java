b835d6cc5a009df49a09d637950a4c1661a5c84e
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-09 10:43:30 +0800

    explore polymorphism on abstract and interface

--- object-oriented-programming-2/src/com/learningjava/oops/level2/interfaces/GameRunner.java
+++ object-oriented-programming-2/src/com/learningjava/oops/level2/interfaces/GameRunner.java
@@ -1,12 +1,22 @@
 package com.learningjava.oops.level2.interfaces;
 
 public class GameRunner {
+
 	public static void main(String[] args) {
+		
+		GamingConsole[] games = { new MarioGame(), new ChessGame()};
+		
+		for (GamingConsole game : games) {
+			game.up();
+			game.down();
+			game.left();
+			game.right();
+		}
 //		GamingConsole game = new MarioGame();
-		GamingConsole game = new ChessGame();
-		game.up();
-		game.down();
-		game.left();
-		game.right();
+//		GamingConsole game = new ChessGame();
+//		game.up();
+//		game.down();
+//		game.left();
+//		game.right();
 	}
 }
