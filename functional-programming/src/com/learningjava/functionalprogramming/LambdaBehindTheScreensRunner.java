d9979afc996d597e8d801f4b958820449de6f644
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-12 15:06:26 +0800

    more functional interfaces exploration

--- functional-programming/src/com/learningjava/functionalprogramming/LambdaBehindTheScreensRunner.java
+++ functional-programming/src/com/learningjava/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -2,6 +2,7 @@ package com.learningjava.functionalprogramming;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -22,11 +23,22 @@ class SystemOutConsumer implements Consumer<Integer> {
 
 }
 
+class NumberSquareMapper implements Function<Integer, Integer> {
+
+	@Override
+	public Integer apply(Integer number) {
+		return number * number;
+	}
+
+}
+
 public class LambdaBehindTheScreensRunner {
 	public static void main(String[] args) {
 
-		List.of(23, 43, 34, 45, 36, 48).stream().filter(n -> n % 2 == 0).forEach(e -> System.out.println(e));
+		List.of(23, 43, 34, 45, 36, 48).stream().filter(n -> n % 2 == 0).map(n -> n * n)
+				.forEach(e -> System.out.println(e));
 
-		List.of(23, 43, 34, 45, 36, 48).stream().filter(new EvenNumberPredicate()).forEach(new SystemOutConsumer());
+		List.of(23, 43, 34, 45, 36, 48).stream().filter(new EvenNumberPredicate()).map(new NumberSquareMapper())
+				.forEach(new SystemOutConsumer());
 	}
 }
