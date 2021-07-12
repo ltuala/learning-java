46cdc3723b0814bc4860bda3ac5ac00af7d1cadc
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-12 10:24:01 +0800

    explore generics and create a getters and setters

---
+++ generics/src/com/learningjava/generics/MyCustomList.java
@@ -0,0 +1,24 @@
+package com.learningjava.generics;
+
+import java.util.ArrayList;
+
+public class MyCustomList<T> {
+	ArrayList<T> list = new ArrayList<>();
+
+	public void addElement(T element) {
+		list.add(element);
+	}
+
+	public void removeElement(T element) {
+		list.remove(element);
+	}
+
+	@Override
+	public String toString() {
+		return "MyCustomList [list=" + list + "]";
+	}
+
+	public T get(int index) {
+		return list.get(index);
+	}
+}
